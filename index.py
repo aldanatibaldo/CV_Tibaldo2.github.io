485a7d9af9c7b7ca90eda23ba6021c4b18ef4645
aldanatibaldo <116826561+aldanatibaldo@users.noreply.github.com>
2024-10-05 17:50:32 -0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+#-------------------------
+# @app.route('/')
+# def principal():
+#     return "Bienvenido a mi sitio web"
+
+# @app.route('/contacto')
+# def contacto():
+#     return "Esta es la página de contacto"
+#-------------------------
+
+@app.route('/doctorado')
+def principal():
+    return render_template('doctorado.html')
+
+@app.route('/docencia')
+def docencia():
+    return render_template('docencia.html')
+
+@app.route('/contacto')
+def contacto():
+    return render_template('contacto.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port = 5017)
+    
